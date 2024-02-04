6697ced15a0713f25f16c3dbb78e14d44c159eb1
JBrunoBarbosa <jbruno356@gmail.com>
2024-02-04 20:35:09 -0300

    fix pipelines

--- src/sources/saleswidget.cpp
+++ src/sources/saleswidget.cpp
@@ -1,6 +1,6 @@
 #include "../headers/saleswidget.h"
-#include "../headers/saleImpl.h"
-#include "../headers/productImpl.h"
+#include "../headers/saleimpl.h"
+#include "../headers/productimpl.h"
 #include "ui_saleswidgetwindow.h"
 
 SalesWidget::SalesWidget(QWidget *parent) :
